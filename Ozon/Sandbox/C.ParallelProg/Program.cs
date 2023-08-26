1a8f39b570b48c1ab0d6a39a8f06437bbf9e2c85
Vasjen <vasjenm@gmail.com>
2023-08-26 20:59:11 +0400

    Complete C Task

--- Ozon/Sandbox/C.ParallelProg/Program.cs
+++ Ozon/Sandbox/C.ParallelProg/Program.cs
@@ -5,20 +5,33 @@ public class Program
     public static void Main(string[] args)
     {
         var queries = convert(Console.ReadLine());
-        someFunction();
+        for (int i = 0; i < queries; i++)
+        {
+            var employeesCount = convert(Console.ReadLine());
+            var employees = Console.ReadLine().Split(' ').Select(p => convert(p)).ToList();
+            var dic = employees.Select((value, index) => new { Index = index, Value = value })
+                .ToDictionary(item => item.Index + 1, item => item.Value);
+            
+            while (dic.Count > 0)
+            {
+                var first = dic.First();
+                
+                dic.Remove(first.Key);
+                var compare = 0;
+                var second = new KeyValuePair<int, int>();
+                while (second.Key == 0)
+                {
+                    second = dic.Where(p => Math.Abs(first.Value - p.Value) == compare).FirstOrDefault();
+                    compare++;
+                }
+                Console.WriteLine($"{first.Key} {second.Key}");
+                dic.Remove(second.Key);
+                
+            }
+            if (i != queries - 1)
+            Console.WriteLine();
+        }
 
     }
     static int convert(string s) => Int32.Parse(s);
-
-    static void someFunction()
-    {
-        var some = Scratch.ReadFromFile("input.txt");
-        int count = convert(Console.ReadLine());
-        var test = "2 1 3 1 1 4";
-        var employees = test.Split(' ').ToDictionary(x => x.IndexOf(x), x => convert(x));
-        foreach (var item in employees)
-        {
-            Console.WriteLine(item.Key + " " + item.Value);
-        }
-    }
 }
