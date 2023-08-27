4b933b5fd7d0cf4fa1e0f99824d445c5e12a3209
Vasjen <vasjenm@gmail.com>
2023-08-27 12:53:20 +0400

    Add structure to contest

---
+++ Ozon/Context.Tests/C.Test/Test.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+
+[TestFixture]
+
+public class Test
+{
+    private static string TestDataPath = Path.GetFullPath("../../../Data/");
+
+        private static IEnumerable<TestCaseData> GetTestData()
+        {
+            string[] inputFiles = Directory.EnumerateFiles(TestDataPath)
+                .Where(file => !file.EndsWith(".a"))
+                .ToArray();
+            foreach (string inputFile in inputFiles)
+            {
+                string expectedFile = Path.GetFileNameWithoutExtension(inputFile) + ".a";
+                string testName = Path.GetFileNameWithoutExtension(inputFile);
+                yield return new TestCaseData(inputFile, expectedFile).SetName(testName);
+            }
+        }
+
+        [TestCaseSource(nameof(GetTestData))]
+        public void TestAppWithFileInput(string inputFile, string expectedFile)
+        {
+            string inputFilePath = Path.Combine(TestDataPath, inputFile);
+            string expectedFilePath = Path.Combine(TestDataPath, expectedFile);
+
+            using (StringReader inputReader = new StringReader(File.ReadAllText(inputFilePath)))
+            using (StringReader expectedOutputReader = new StringReader(File.ReadAllText(expectedFilePath)))
+            using (StringWriter outputWriter = new StringWriter())
+            {
+                Console.SetIn(inputReader);
+                Console.SetOut(outputWriter);
+
+                Program.Main(new string[0]);
+
+                string expectedOutput = expectedOutputReader.ReadToEnd();
+                string actualOutput = outputWriter.ToString();
+                expectedOutput = expectedOutput.Replace("\r\n", "\n");
+                actualOutput = actualOutput.Replace("\r\n", "\n");
+                Assert.AreEqual(expectedOutput, actualOutput);
+            }
+        }
+    }
+
+
---
+++ Ozon/Sandbox/E.Report/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Channels;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var queries = convert(Console.ReadLine());
+        for (var i = 0; i < queries; i++)
+        {
+            Console.ReadLine();
+            var res = isContinued(Console.ReadLine().Split(' ').Select(p => convert(p)).ToList());
+            Console.WriteLine(res ? "YES" : "NO");
+        }
+       
+    
+    }
+    static int convert(string s) => Int32.Parse(s);
+
+    static bool isContinued(List<int> values)
+    {
+        Dictionary<int, int> dic = values.Select((value, index) => new { Index = index, Value = value })
+            .ToDictionary(item => item.Index + 1, item => item.Value);
+        var groupedValues = dic.GroupBy(p => p.Value)
+            .ToDictionary(p => p.Key, p => p.Select(p => p.Key)
+                .ToList());
+        foreach (var item in groupedValues.Values)
+        {
+            if (item.Count > 1)
+            {
+                for (int i = 0; i < item.Count - 1; i++)
+                {
+                    if( Math.Abs(item[i] - item[i + 1]) != 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+           
+        }
+        
+        return true;
+    }
+
+
+
+}
